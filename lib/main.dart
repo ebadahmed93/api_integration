3b73ea7da172ebe4a9b332979b591ec040410622
ebadahmed93 <ebad.ahmed@tpsonline.com>
2022-02-16 15:46:31 +0500

    Post API complete

--- lib/main.dart
+++ lib/main.dart
@@ -53,13 +53,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool showingList = false;
+  int showList = 0; // 0 get single data, 1 get all data,
+  // 2 show single posted data, 3 show all posted data
+
   late Future<DummyData> getSingle;
   late Future<DummyData> postSingle;
   late Future<List<DummyData>> getAll;
   late Future<List<DummyData>> postAll;
 
   late Future<DummyData> genericData;
+
+  final _formKey = GlobalKey<FormState>();
+  final controller1 = TextEditingController();
+  final controller2 = TextEditingController();
+  final controller3 = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller1.dispose();
+    controller2.dispose();
+    controller3.dispose();
+  }
 
   @override
   void initState() {
@@ -108,30 +123,56 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                    onPressed: postSingleData(),
+                    onPressed: () {
+                      postSingleData();
+                    },
                     child: Text('PostSingleData'),
                   ),
                 )),
-                Expanded(
+                /*Expanded(
                     child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                    onPressed: postAllData(),
+                    onPressed:(){ postAllData();},
                     child: Text('PostAllData'),
                   ),
-                ))
+                ))*/
               ],
             ),
             Flexible(
-              child:
-                  // wrap with a scrollable widget
-                  showingList ? showListData() : showSingleData(),
+              child: getWidget(),
+              // wrap with a scrollable widget
             )
           ],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<DummyData> sendSingleData(DummyData data) async {
+    final response = await http.post(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'userId': data.userId,
+        'title': data.title,
+        'body': data.body,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+
+      return DummyData.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create album.');
+    }
   }
 
   Future<DummyData> fetchSingleData() async {
@@ -152,7 +193,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<List<DummyData>> fetchAllData() async {
     final response =
-        await http.get('https://jsonplaceholder.typicode.com/posts');
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
       return jsonResponse.map((data) => DummyData.fromJson(data)).toList();
@@ -162,12 +203,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   postAllData() {
+   // showPopup(false);
     /*setState(() {
       genericData = getSingle;
     });*/
   }
 
   postSingleData() {
+    showPopup();
     /*   setState(() {
       showingList = false;
     });*/
@@ -175,15 +218,59 @@ class _MyHomePageState extends State<MyHomePage> {
 
   getSingleData() {
     setState(() {
-      showingList = false;
+      showList = 0;
     });
   }
 
   getAllData() {
     setState(() {
-      showingList = true;
-      showingList = true;
+      showList = 1;
     });
+  }
+
+  Widget showPostAllData() {
+    return FutureBuilder<List<DummyData>>(
+      future: postAll,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<DummyData>? data = snapshot.data;
+          return ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: data?.length,
+              /*physics: const NeverScrollableScrollPhysics(),*/
+              itemBuilder: (BuildContext context, int index) {
+                return Column(children: [
+                  Text(snapshot.data![index].title),
+                  Text(snapshot.data![index].userId.toString())
+                ]);
+              });
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default show a loading spinner.
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  Widget showPostSingleData() {
+    return FutureBuilder<DummyData>(
+      future: postSingle,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Column(children: [
+            Text(snapshot.data!.title),
+            Text(snapshot.data!.userId.toString())
+          ]);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        // By default, show a loading spinner.
+        return const CircularProgressIndicator();
+      },
+    );
   }
 
   Widget showSingleData() {
@@ -229,5 +316,91 @@ class _MyHomePageState extends State<MyHomePage> {
         return CircularProgressIndicator();
       },
     );
+  }
+
+  showPopup() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Stack(
+              children: <Widget>[
+                Positioned(
+                  right: -40.0,
+                  top: -40.0,
+                  child: InkResponse(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: CircleAvatar(
+                      child: Icon(Icons.close),
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: controller1,
+                          decoration:
+                              const InputDecoration(hintText: 'Enter Title'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: controller2,
+                          decoration:
+                              const InputDecoration(hintText: 'Enter Body'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              DummyData data = DummyData(
+                                  userId: 12,
+                                  id: 0,
+                                  title: controller1.text,
+                                  body: controller2.text);
+
+                              // do something with the form data
+                              showList = 2;
+                              setState(() {
+                                postSingle = sendSingleData(data);
+                                Navigator.pop(context);
+                              });
+                            }
+                          },
+                          child: const Text('Create Data'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  Widget getWidget() {
+    if (showList == 0) {
+      return showSingleData();
+    } else if (showList == 1) {
+      return showListData();
+    } else if (showList == 2) {
+      return showPostSingleData();
+    } else if (showList == 3) {
+      return showPostAllData();
+    } else {
+      return showSingleData();
+    }
   }
 }
